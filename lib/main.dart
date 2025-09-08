31a8cac25209bcf1cf90bf3e57865c08808ead0d
rana_inthemood <ro7alimansour@gmail.com>
2025-09-08 22:11:06 +0300

    app router - signin view- custom liquid glass snackbar app lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/firebase_options.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'features/Auth/controller.dart';
-import 'features/Auth/view/login_view.dart';
-import 'features/Auth/view/signup_view.dart';
-import 'features/home/home_view.dart';
+import 'core/app_lifecycle_handler.dart';
+import 'core/routing/app_router.dart';
 import 'features/notifcation audio state/audio_handler.dart';
 
 late final AudioHandler audioHandler;
@@ -25,78 +22,21 @@ void main() {
   });
 }
 
-class MyApp extends ConsumerStatefulWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
-
+  
   @override
-  ConsumerState<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends ConsumerState<MyApp> with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      ref.refresh(userStreamProvider);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, ref, child) {
-        final userStream = ref.watch(userStreamProvider);
-
-        return MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            textTheme: GoogleFonts.robotoTextTheme(Theme.of(context).textTheme),
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          ),
-          routerConfig: GoRouter(
-            initialLocation: '/signup',
-            routes: [
-              GoRoute(
-                path: '/signup',
-                builder: (context, state) => const SignUpScreen(),
-              ),
-              GoRoute(
-                path: '/login',
-                builder: (context, state) => const LoginScreen(),
-              ),
-              GoRoute(
-                path: '/home',
-                builder: (context, state) => const HomeScreen(),
-              ),
-            ],
-            redirect: (context, state) {
-              final user = userStream.value;
-              final currentPath = state.uri.toString();
-              if (user == null &&
-                  currentPath != '/login' &&
-                  currentPath != '/signup') {
-                return '/signup';
-              } else if (user != null &&
-                  (currentPath == '/login' || currentPath == '/signup')) {
-                return '/home';
-              }
-              return null;
-            },
-          ),
-        );
-      },
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AppLifecycleHandler(
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          textTheme: GoogleFonts.robotoTextTheme(Theme.of(context).textTheme),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        routerConfig: createRouter( ref),
+      ),
     );
   }
 }
